d80c005ac06fc36f26227c9c830a266dd70c7b65
kmk <manekingz@gmail.com>
2022-11-06 02:15:52 +0900

    Add comments.

--- lib/src/events/pluto_relayout_event.dart
+++ lib/src/events/pluto_relayout_event.dart
@@ -1,5 +1,7 @@
 import 'events.dart';
 
+/// Event to reset the layout
+/// when the size of the parent widget of [Pluto Layout] is changed.
 class PlutoRelayoutEvent extends PlutoLayoutEvent {
   const PlutoRelayoutEvent();
 }
